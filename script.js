ecf1783216fcbfaa20751fecf14a3d0eaa02db5a
unknown <alex_vsz@outlook.com>
2025-04-07 23:10:29 -0600

    El primer commit de la actividad Css con Bootstrap

---
+++ script.js
@@ -0,0 +1,32 @@
+
+console.log("Hola Mundo!");
+
+const colores = ["azul","rojo","verde"];
+console.log(colores[0]);
+colores[2] = 22;
+colores.forEach((color)=>console.log(color));
+
+// let es como var
+const alumnos = {name: "Luis"};
+alumnos.edad = 20;
+console.log(alumnos.edad);
+
+alumno = [];
+
+const saludo = ()=> {
+    console.log("Hola++")
+};
+
+saludo();
+
+function msg() {
+    console.log("Halalalala");
+};
+
+function msg(name) {
+    console.log("Hola " + name);
+};
+
+msg();
+msg("Diego");
+
